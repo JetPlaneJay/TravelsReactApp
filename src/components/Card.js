460e67a4f573f704847929907a81d249e6452050
unknown <jacob.tabares90@gmail.com>
2022-04-15 15:46:57 -0700

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Card(props) {
+
+    return (
+        <div className="card">
+            <img src={`${props.imageUrl}`}
+            className="card--image" />
+            <article>
+                <section>
+                    <p className="card--country">{props.location}  
+                        <a href="{props.googleMapsUrl}">View on Google Maps</a> 
+                    </p>
+                </section>
+                <section>
+                <h1 className="card--title">{props.title}</h1>
+                <p><strong>{props.startDate} - {props.endDate}</strong></p>
+                </section>
+                <section>
+                    <p>{props.description}</p>
+                </section>
+            </article>
+        </div>
+    )
+}
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src="../images/logo.png" className="nav--logo" />
+            <h2>my travel journal.</h2>
+        </nav>
+    )
+}
+
